6a333e015be910eec1e158d112fe0625b27fcef7
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-05-16 15:44:42 +0300

    Adds GlobalStyle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { RecipeList } from './RecipeList';
 import recipes from '../recipes.json';
+import GlobalStyle from './GlobalStyle';
 
 export const App = () => {
   return (
     <div>
       <RecipeList items={recipes} />
+      <GlobalStyle />
     </div>
   );
 };
